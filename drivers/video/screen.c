9e1da6df0f6c764dbfb42b853d258b8bb3e024a6
Filip Kania <fkania2@gmail.com>
2021-01-12 20:30:49 +0100

    Video: Added SPrint function and SPrint_change_color

---
+++ drivers/video/screen.c
@@ -0,0 +1,105 @@
+#include "./screen.h"
+#include "ports.h"
+
+/* Declaration of private functions */
+int get_cursor_offset();
+void set_cursor_offset(int offset);
+int print_char(char c, int col, int row, char attr);
+int get_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+int default_color = WHITE_ON_BLACK;
+
+/**********************************************************
+ * Public Kernel API functions                            *
+ **********************************************************/
+
+void sprint_at(char *message, int col, int row) {
+    int offset;
+    if (col >= 0 && row >= 0)
+        offset = get_offset(col, row);
+    else {
+        offset = get_cursor_offset();
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
+
+    int i = 0;
+    while (message[i] != 0) {
+        offset = print_char(message[i++], col, row, default_color);
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
+}
+
+void sprint(char *message) {
+    sprint_at(message, -1, -1);
+}
+
+void sprint_change_color(int color) { 
+    default_color = color;
+}
+
+/**********************************************************
+ * Private kernel functions                               *
+ **********************************************************/
+
+int print_char(char c, int col, int row, char attr) {
+    unsigned char *vidmem = (unsigned char*) VIDEO_ADDRESS;
+    if (!attr) attr = default_color;
+
+    if (col >= MAX_COLS || row >= MAX_ROWS) {
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-2] = 'E';
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-1] = RED_ON_WHITE;
+        return get_offset(col, row);
+    }
+
+    int offset;
+    if (col >= 0 && row >= 0) offset = get_offset(col, row);
+    else offset = get_cursor_offset();
+
+    if (c == '\n') {
+        row = get_offset_row(offset);
+        offset = get_offset(0, row+1);
+    } else {
+        vidmem[offset] = c;
+        vidmem[offset+1] = attr;
+        offset += 2;
+    }
+    set_cursor_offset(offset);
+    return offset;
+}
+
+int get_cursor_offset() {
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    int offset = port_byte_in(REG_SCREEN_DATA) << 8;
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    offset += port_byte_in(REG_SCREEN_DATA);
+    return offset * 2;
+}
+
+void set_cursor_offset(int offset) {
+    offset /= 2;
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+void clear_screen() {
+    int screen_size = MAX_COLS * MAX_ROWS;
+    int i;
+    char *screen = VIDEO_ADDRESS;
+
+    for (i = 0; i < screen_size; i++) {
+        screen[i*2] = ' ';
+        screen[i*2+1] = default_color;
+    }
+    set_cursor_offset(get_offset(0, 0));
+}
+
+
+int get_offset(int col, int row) { return 2 * (row * MAX_COLS + col); }
+int get_offset_row(int offset) { return offset / (2 * MAX_COLS); }
+int get_offset_col(int offset) { return (offset - (get_offset_row(offset)*2*MAX_COLS))/2; }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,7 @@
-#include "../drivers/video/ports.h"
+#include "../drivers/video/screen.h"
 
 void main() {
-    port_byte_out(0x3d4, 14);
-    int position = port_byte_in(0x3d5);
-    position = position << 8;
-
-    port_byte_out(0x3d4, 15);
-    position += port_byte_in(0x3d5);
-
-    int offset_from_vga = position * 2;
-
-    char *vga = 0xb8000;
-    vga[offset_from_vga] = 'A'; 
-    vga[offset_from_vga+1] = 'E';
+    clear_screen();
+    sprint_change_color(RED_ON_WHITE);
+    sprint("qwertyuiop");
 }
