1b25c0ae5e1c3f9e072070e4cb84deed86249f7b
Filip Kania <fkania2@gmail.com>
2021-02-24 11:01:06 +0100

    Kernel: changed ASSERT and ASSERT_NOT_REACHED messages

--- kernel/include/stdlib.h
+++ kernel/include/stdlib.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define ASSERT(EX) (void)(EX || (__internal_assert(#EX, __FILE__, __LINE__), 0))
-#define ASSERT_NOT_REACHED() (void)(__internal_assert("REACHED", __FILE__, __LINE__), 0)
+#define ASSERT(EX) (void)(EX || (__internal_assert(#EX, __FILE__, __LINE__, __FUNCTION__), 0))
+#define ASSERT_NOT_REACHED() (void)(__internal_assert("was reached. Assertion", __FILE__, __LINE__, __FUNCTION__), 0)
 
 void __ABORT();
 
 void KERNEL_PANIC();
 
-void __internal_assert(const char *msg, const char *file, int line);
+void __internal_assert(const char *msg, const char *file, int line, const char *function);
--- kernel/stdlib/assert.cc
+++ kernel/stdlib/assert.cc
@@ -2,12 +2,24 @@
 #include <terminal.h>
 #include <string.h>
 
-void __internal_assert(const char *msg, const char *file, int) {
-    char str[] = "\nAssertion failed: ";
+void __internal_assert(const char *msg, const char *file, int line, const char *function) {
+    char str[150] = "\n";
+    char line_str[5];
+
+    strcat(str, function);
+    strcat(str, "(): ");
+
     strcat(str, file);
-    strcat(str, ": ");
+    strcat(str, ":");
+
+    itoa(line, line_str);
+    strcat(str, line_str);
+
+    strcat(str, " - assertion ");
 
     strcat(str, msg);
+
+    strcat(str, " failed.");
 
     Terminal::write(str);
 
