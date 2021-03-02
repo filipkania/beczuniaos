539cfc0ba38f3bff6c098160f1cb603b038d7e2f
Filip Kania <fkania2@gmail.com>
2021-03-02 09:37:32 +0100

    Kernel: formatstr now look better with __internal__format helper

--- kernel/string/formatstr.cc
+++ kernel/string/formatstr.cc
@@ -6,21 +6,28 @@ char *formatstr(const char *format, ...) {
     va_list params;
     va_start(params, format);
 
+    return __internal__format(format, params);
+}
+
+char *__internal__format(const char* format, va_list params) {
     static char str[512];
+    bool canHaveNextArgument = true;
     size_t location = 0;
 
     for (size_t i = 0; i < 512; i++)
         str[i] = '\0';
 
     for (size_t i = 0; i < strlen(format); i++) {
-        if (format[i] == '{' && format[i + 1] == '}') {
+        if (format[i] == '{' && format[i + 1] == '}' && canHaveNextArgument) {
             const char *arg = va_arg(params, char *);
 
-            if (arg != (const char *) nullptr) {
+            if (arg != nullptr) {
                 location += strcat(str, arg);
-            } else
+            } else {
                 location += strcat(str, "{}");
-        } else if (format[i - 1] == '{' && format[i] == '}')
+                canHaveNextArgument = false;
+            }
+        } else if (format[i - 1] == '{' && format[i] == '}' && canHaveNextArgument)
             continue;
         else {
             str[location] = format[i];
@@ -31,6 +38,8 @@ char *formatstr(const char *format, ...) {
     auto length = strlen(str);
     str[length] = '\0';
     ASSERT(length < 512);
+
+    va_end(params);
 
     return str;
 }
